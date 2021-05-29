64c6c42aa01cc9f77bb1e7eeeb54adfecf95f12c
Łukasz Jackiewicz <lukasz.jackiewicz94@gmail.com>
2021-05-30 00:44:52 +0200

    Add possibility to organising cards within decks

--- data/schema.sql
+++ data/schema.sql
@@ -1,6 +1,20 @@
+drop table if exists decks;
+create table decks (
+    id      integer     primary key,
+    name    text        not null
+);
+
 drop table if exists cards;
 create table cards (
-    id      integer     primary key autoincrement,
+    id      integer     primary key,
     front   text        not null,
-    back    text        not null
+    back    text        not null,
+    deck_id integer     not null,
+
+    foreign key (deck_id)
+        references decks(id)
+            on update cascade
+            on delete cascade
 );
+
+insert into decks (name) values ('default');
